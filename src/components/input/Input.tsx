4dc0bd0fe25a0056c8270b8ccf49f8d45bf6234a
Festus Phillip <st.festus4cruise@gmail.com>
2025-12-31 20:27:02 +0100

    established the individual tax caculation logic to handle users task that falls into the indiv idual section

--- src/components/input/Input.tsx
+++ src/components/input/Input.tsx
@@ -1,10 +1,18 @@
-const Input = ({ value, onChnage, placeholder, title }) => {
+
+type props = {
+    value: number;
+    onChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
+    placeholder: string;
+    title: string;
+}
+
+const Input = ({ value, onChange, placeholder, title }: props) => {
   return (
     <div className="flex flex-row flex-wrap items-center gap-5 ">
       <label title="monthly salary x 12 months">{title}</label>
       <input
         value={value}
-        onChange={onChnage}
+        onChange={onChange}
         placeholder={placeholder}
         type="number"
         className="boder-none outline-none bg-[#80808018] p-2 lg:p-4 rounded-md lg:w-[80%] md:w-[60%]"
